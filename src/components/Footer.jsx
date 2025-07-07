9fd148408e6d7998f616c6fb400f2f1693f1fa5f
0tieno <ronvi035@gmail.com>
2025-07-07 20:42:53 +0300

    Fix: remove background color from Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="bg-gray-900 text-gray-400 text-sm py-4 text-center border-t border-gray-800">
+    <footer className=" text-gray-400 text-sm py-4 text-center border-t border-gray-800">
       <p>
         © {new Date().getFullYear()} Profile Upload App by{" "}
         <a
